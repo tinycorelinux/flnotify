95174b6c28da873f201b385e7a182fe1e57154f2
Lauri Kasanen <curaga@operamail.com>
2011-12-10 19:39:43 +0200

    Smaller build opts

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 g++ -o flnotify -Os -I/opt/fltk/include -L/opt/fltk/lib \
 -Wl,-rpath,/opt/fltk/lib -I/usr/X11R7/include \
 -lfltk -fno-rtti -fno-exceptions flnotify.cxx \
--s -Wall -fomit-frame-pointer
+-s -Wall -fomit-frame-pointer -Wextra -ffunction-sections \
+-fdata-sections -Wl,-O1 -Wl,-gc-sections
 
 strip -R .comment -R .gnu.version flnotify
