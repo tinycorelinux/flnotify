b9929575c4a4241d9c6461048022d37ce80bd15a
juanitotc <john.frankish@outlook.com>
2021-11-22 08:59:28 +0000

    juanito: rearrange build flags

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-g++ -o flnotify -Os -I/opt/fltk/include -L/opt/fltk/lib \
--Wl,-rpath,/opt/fltk/lib -I/usr/X11R7/include \
--lfltk -fno-rtti -fno-exceptions flnotify.cxx \
+g++ -o flnotify -Os -I/usr/local/include/FL -L/usr/local/lib \
+-Wl,-rpath,/usr/local/lib -I/usr/local/include \
+-lfltk -march=i486 -mtune=i686 -fno-rtti -fno-exceptions flnotify.cxx \
 -s -Wall -fomit-frame-pointer -Wextra -ffunction-sections \
 -fdata-sections -Wl,-O1 -Wl,-gc-sections
 
-strip -R .comment -R .gnu.version flnotify
+sstrip -R .comment -R .gnu.version flnotify
